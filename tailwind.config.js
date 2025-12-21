9da0ace07f5c155a368b18e600ba27936023fe13
phongha <havanphong784@gmail.com>
2025-12-21 15:04:13 +0700

    header complete

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "rgba(255, 255, 255, 0.15)",
+        bg_color: "#020103",
+        border_color: "rgba(255, 255, 255, 0.15)",
+        btn_color: "rgba(140, 69, 255, 0.40)",
+      },
+    },
+  },
+  plugins: [],
+};
